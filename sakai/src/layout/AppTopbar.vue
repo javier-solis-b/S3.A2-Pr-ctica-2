45d400082a404773bc55389d5a2cad7fd0420d53
Javier <javijsol96@gnail.com>
2023-11-08 11:02:24 -0600

    version 1

--- sakai/src/layout/AppTopbar.vue
+++ sakai/src/layout/AppTopbar.vue
@@ -64,7 +64,7 @@ const isOutsideClicked = (event) => {
     <div class="layout-topbar">
         <router-link to="/" class="layout-topbar-logo">
             <img :src="logoUrl" alt="logo" />
-            <span>Facultad de negocios UNACH</span>
+            <span>Huacaleros con sabor</span>
         </router-link>
 
         <button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle()">
@@ -76,14 +76,7 @@ const isOutsideClicked = (event) => {
         </button>
 
         <div class="layout-topbar-menu" :class="topbarMenuClasses">
-            <button @click="onTopBarMenuButton()" class="p-link layout-topbar-button">
-                <i class="pi pi-calendar"></i>
-                <span>Calendar</span>
-            </button>
-            <button @click="onTopBarMenuButton()" class="p-link layout-topbar-button">
-                <i class="pi pi-user"></i>
-                <span>Profile</span>
-            </button>
+            
             <button @click="onSettingsClick()" class="p-link layout-topbar-button">
                 <i class="pi pi-cog"></i>
                 <span>Settings</span>
--- sakai/src/views/uikit/FormLayout.vue
+++ sakai/src/views/uikit/FormLayout.vue
@@ -1,50 +1,64 @@
+<template>
+  <div class="grid">
+    <div class="col-12 md:12">
+      <div class="card p-fluid">
+        <div class="card-body">
+          <h4 class="card-title">Lista de Platillos</h4>
+          <div class="table-responsive">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Descripción</th>
+                  <th>Precio</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="platillo in platillos" :key="platillo.id">
+                  <td>{{ platillo.nombre }}</td>
+                  <td>{{ platillo.descripcion }}</td>
+                  <td>{{ formatCurrency(platillo.precio) }}</td>
+                  <td>
+                    <button class="btn btn-primary" @click="editarPlatillo(platillo)">Editar</button>
+                    <button class="btn btn-danger" @click="eliminarPlatillo(platillo)">Eliminar</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-success" @click="agregarPlatillo">Agregar Platillo</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup>
 import { ref } from 'vue';
-import Button from 'primevue/button';
 
-const dropdownItems = ref([
-    { name: 'Option 1', code: 'Option 1' },
-    { name: 'Option 2', code: 'Option 2' },
-    { name: 'Option 3', code: 'Option 3' }
+const platillos = ref([
+  { id: 1, nombre: 'Platillo 1', descripcion: 'Descripción del Platillo 1', precio: 10.99 },
+  { id: 2, nombre: 'Platillo 2', descripcion: 'Descripción del Platillo 2', precio: 15.99 },
+  { id: 3, nombre: 'Platillo 3', descripcion: 'Descripción del Platillo 3', precio: 8.99 },
 ]);
 
-const dropdownItem = ref(null);
+const formatCurrency = (value) => {
+  return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+};
 
-const calendarValue = ref(null);
+const agregarPlatillo = () => {
+  // Lógica para agregar un nuevo platillo a la lista
+};
+
+const editarPlatillo = (platillo) => {
+  // Lógica para editar un platillo
+};
+
+const eliminarPlatillo = (platillo) => {
+  // Lógica para eliminar un platillo
+};
 </script>
 
-<template>
-    <div class="grid">
-        <div class="col-12 md:col-12">
-            <div class="card p-fluid">
-                <h5>GENERAR EL RFC DE UNA PERSONA</h5>
-                <div class="field">
-                    <label for="name1">Nombre(s):</label>
-                    <InputText id="name1" type="text" />
-                </div>
-                <div class="field">
-                    <label for="ape1">Apellido Paterno:</label>
-                    <InputText id="ape1" type="text" />
-                </div>
-                <div class="field">
-                    <label for="ape2">Apellido Materno:</label>
-                    <InputText id="ape2" type="text" />
-                </div>
-                <div class="field">
-                    <label for="calendar">Fecha:</label>
-                    <Calendar :showIcon="true" :showButtonBar="true" v-model="calendarValue"></Calendar>
-                </div>
-                <div class="field">
-                    <label for="rfc">RFC:</label>
-                    <InputText id="rfc" type="text" />
-                </div>
-                <div class="col-6 md:col-7">
-                    <Button label="GENERAR" />
-                </div>
-            </div>
-            
-        </div>
-       
-      
-    </div>
-</template>
